62f8f7edb82ef2533c3d93c827816a2c0b9a3329
Andrew Herman <aherman723@gmail.com>
2023-12-02 14:30:16 -0500

    built front end for contests lists

---
+++ vue/src/views/ContestsView.vue
@@ -0,0 +1,18 @@
+<template>
+    <contests-vue />
+</template>
+
+<script>
+
+import ContestsVue from '../components/Contests.vue'
+
+export default {
+    components: {
+        ContestsVue
+    }
+}
+</script>
+
+<style>
+
+</style>
