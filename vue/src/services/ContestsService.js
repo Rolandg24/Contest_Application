77f4161397c810141fc0dce1ca2ea6999e009839
Forrest Yankey <forrestyankey@gmail.com>
2023-12-03 12:21:49 -0500

    created axios method for getParticipants

--- vue/src/services/ContestsService.js
+++ vue/src/services/ContestsService.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 
+const http = axios.create({
+    baseURL: "http://localhost:9000"
+})
+
 export default{
 
     fetchContests(){
         return axios.get('/contests');
+    },
+
+    fetchParticipantsById() {
+        return http.get(`/contests/${contestId}`);
     }
-
-
 }
