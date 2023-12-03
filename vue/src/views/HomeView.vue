fa4b6b2e5d92971a3e74dc74dae2b70b89ebbb5c
Roland Garza <garza.roland24@gmail.com>
2023-12-03 13:59:11 -0500

    Pushing CSS

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <img class="Logo" src="../assets/1st_Draft_Logo.png" alt="Draft Logo of Man Climbing Mountain">
     <h1>Home</h1>
     <p>You must be authenticated to see this</p>
   </div>
@@ -9,3 +10,20 @@
 export default {
 };
 </script>
+
+<style>
+body {
+  background-color: #FFF7D4
+}
+
+.Logo {
+display: block;
+margin-left: auto;
+margin-right: auto;
+width: 50%
+}
+
+.home {
+  
+}
+</style>
