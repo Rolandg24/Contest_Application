8e390adadd88feef56b82702f26313606b5beae4
Roland Garza <garza.roland24@gmail.com>
2023-12-05 10:17:15 -0500

    Pushing NewcontestForm changes

--- vue/src/components/NewContestForm.vue
+++ vue/src/components/NewContestForm.vue
@@ -1,51 +1,31 @@
 <template>
+  <!-- <h1>i</h1> -->
   <form v-on:submit.prevent="submitForm">
     <!-- Name Field -->
     <div class="mb-3">
       <label for="inputName" class="form-label">Name</label>
-      <input
-        type="text"
-        class="form-control"
-        id="inputName"
-        placeholder="Enter name"
-        v-model="newContest.contestName"
-      />
+      <input type="text" class="form-control" id="inputName" placeholder="Enter name" v-model="newContest.contestName" />
     </div>
 
     <!-- Description Field -->
     <div class="mb-3">
       <label for="inputDescription" class="form-label">Description</label>
-      <textarea
-        class="form-control"
-        id="inputDescription"
-        rows="3"
-        placeholder="Enter description"
-        v-model="newContest.contestDescription"
-      ></textarea>
+      <textarea class="form-control" id="inputDescription" rows="3" placeholder="Enter description"
+        v-model="newContest.contestDescription"></textarea>
     </div>
 
     <!-- Date and Time Field -->
     <div class="mb-3">
       <label for="inputLocation" class="form-label">Date and Time</label>
-      <input
-        type="text"
-        class="form-control"
-        id="inputLocation"
-        placeholder="Enter date and time"
-        v-model="newContest.dateAndTime"
-      />
+      <input type="text" class="form-control" id="inputLocation" placeholder="Enter date and time"
+        v-model="newContest.dateAndTime" />
     </div>
 
     <!-- Location Field -->
     <div class="mb-3">
       <label for="inputLocation" class="form-label">Location</label>
-      <input
-        type="text"
-        class="form-control"
-        id="inputLocation"
-        placeholder="Enter location"
-        v-model="newContest.contestLocation"
-      />
+      <input type="text" class="form-control" id="inputLocation" placeholder="Enter location"
+        v-model="newContest.contestLocation" />
     </div>
 
     <!-- Submit Button -->
@@ -57,40 +37,53 @@
 import ContestsService from "../services/ContestsService";
 
 export default {
-    props: {
-        contest: {
-            type: Object, 
-            required: true
-        }
-    },
+  props: {
+    // contest: {
+    //     type: Object, 
+    //     required: false
+    // }
+  },
   data() {
     return {
+      contest: {},
       newContest: {
-        contestId: this.contest.contestId,
-        contestName: this.contest.contestName,
-        contestDescription: this.contest.contestDescription,
-        dateAndTime: this.contest.dateAndTime,
-        contestLocation: this.contest.contestLocation,
-      },
+        contestId: '',
+        contestName: '',
+        contestDescription: '',
+        dateAndTime: '',
+        contestLocation: '',
+      }
+      // newContest: {
+      //   contestId: this.contest.contestId,
+      //   contestName: this.contest.contestName,
+      //   contestDescription: this.contest.contestDescription,
+      //   dateAndTime: this.contest.dateAndTime,
+      //   contestLocation: this.contest.contestLocation,
+      // },
     };
   },
   methods: {
+    getContest(id) {
+      ContestsService.fetchContestById(id)
+        .then(response => {
+          this.contest = response.data;
+        })
+    },
     submitForm() {
-      if (this.newContest.contestId === 0) {
+      if (this.newContest.contestId == 0) {
         ContestsService.createNewContest(this.newContest)
           .then((response) => {
-            console.log("hello?");
             if (response.status == 201) {
               this.$router.push({ name: "contests" });
             }
           })
           .catch((error) => {
-            this.handleErrorResponse(error);
+            this.handleErrorResponse(error, "creating");
           });
       } else {
         ContestsService.updateContest(this.newContest)
           .then((response) => {
-            if (response.status === 200) {
+            if (response.status == 200) {
               this.$router.push({ name: "contests" });
             }
           })
@@ -119,6 +112,9 @@ export default {
         );
       }
     },
+    created(){
+      this.getContest(this.$route.params.contestId);
+    }
   }
 };
 </script>
--- vue/src/views/ContestsView.vue
+++ vue/src/views/ContestsView.vue
@@ -1,21 +1,28 @@
 <template>
     <div>
-    <router-link v-bind:to="{name: 'NewContest'}">Add Contest</router-link>
-    <contests-vue />
-</div>
+        <router-link v-bind:to="{ name: 'NewContest' }">Add Contest</router-link>
+        <contests-vue />
+    </div>
 </template>
 
 <script>
 
 import ContestsVue from '../components/Contests.vue'
-
+import ContestsService from '../services/ContestsService';
 export default {
     components: {
         ContestsVue
-    }
+    },
+    methods: {
+        getContest(id) {
+            ContestsService.fetchContestById(id)
+                .then(response => {
+                    this.contest = response.data;
+                })
+        }
+
+    },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
