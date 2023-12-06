3b34a02490bfcdcaae6df6a3851263bb7c0d5af6
Roland Garza <garza.roland24@gmail.com>
2023-12-06 13:48:29 -0500

    Pushing Data

--- java/database/data.sql
+++ java/database/data.sql
@@ -82,7 +82,7 @@ VALUES
 ('Code Connoisseurs', 'Connoisseurs of clean code', 6, 84, 2),
 ('Visionary Vanguards', 'At the forefront of innovation', 4, 75, 2),
 ('Tech Transformers II', 'Transforming tech landscapes', 7, 97, 2),
-('Innovative Impact', 'Making an innovative impact', 5, 82, 2);
+('Innovative Impact', 'Making an innovative impact', 5, 82, 2),
 ('Innovative Testing Measures', 'Making an innovative impact', 5, 82, 4);
 
 INSERT INTO time_slots (contest_id, participant_id, time_slot)
