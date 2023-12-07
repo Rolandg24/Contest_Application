5ec1ac339a5b29fefd97727caa5e1b79d4ff4a2c
Forrest Yankey <forrestyankey@gmail.com>
2023-12-07 07:24:45 -0500

    edit layout of Contest.vue

--- vue/src/components/Contests.vue
+++ vue/src/components/Contests.vue
@@ -1,85 +1,155 @@
 <template>
-    <div class="ContestInfo">
-        <div class="ContestContainer" v-for="contest in $store.state.contests" v-bind:key="contest.contestId">
-            <h1>{{ contest.contestName }}</h1>
-            <p>{{ contest.contestLocation }}</p>
-            <p>{{ contest.dateAndTime }}</p>
-            <div>
-                <router-link
-                    v-bind:to="{ name: 'participants', params: { contestId: contest.contestId } }">Participants</router-link>
-            </div>
-            <div>
-                <a href="#" v-on:click="$router.push({ name: 'Schedule', params: {contestId: contest.contestId} })">Schedule</a> <!--TO DO DECIDE NEW WINDOW OR POPULATE INSIDE CONTAINER-->
-            </div>
-            <div>
-                <a href="#" v-on:click="$router.push({name: 'AddSchedule', params: {contestId: contest.contestId}})">Add Schedule</a>
-            </div>
-            <div>
-                <a href="#" v-on:click="$router.push({ name: 'UpdateContest', params: {contestId: contest.contestId} })">Update</a>
-            </div>
-            <div>
-                <a href="#" v-on:click="deleteContest(contest.contestId)">Delete</a>
-            </div>
-            <div>
-                <a href="#" v-on:click="$router.push({ name: 'OverallScores', params: {contestId: contest.contestId} })">Scores</a>
-            </div>
+  <div class="ContestInfo">
+    
+    <div class="card" style="width: 18rem" v-for="contest in $store.state.contests"
+      v-bind:key="contest.contestId">
+      <img src="../assets/1st_Draft_Logo.png" class="card-img-top" alt="placeholder" />
+      <div class="card-body">
+        <h5 class="card-title">{{ contest.contestName }}</h5>
+        <p class="card-text">{{ contest.contestLocation }}</p>
+        <p class="card-text">{{ contest.dateAndTime }}</p>
+        <div class="btn-container">
+            <a href="#" class="btn btn-primary">Details</a>
+            <a href="#" class="btn btn-primary">Register</a>
         </div>
+      </div>
     </div>
+
+    <!-- <div
+      class="ContestContainer"
+      v-for="contest in $store.state.contests"
+      v-bind:key="contest.contestId"
+    >
+      <h1>{{ contest.contestName }}</h1>
+      <p>{{ contest.contestLocation }}</p>
+      <p>{{ contest.dateAndTime }}</p>
+      <div>
+        <router-link
+          v-bind:to="{
+            name: 'participants',
+            params: { contestId: contest.contestId },
+          }"
+          >Participants</router-link
+        >
+      </div>
+      <div>
+        <a
+          href="#"
+          v-on:click="
+            $router.push({
+              name: 'Schedule',
+              params: { contestId: contest.contestId },
+            })
+          "
+          >Schedule</a
+        >
+       
+      </div>
+      <div>
+        <a
+          href="#"
+          v-on:click="
+            $router.push({
+              name: 'AddSchedule',
+              params: { contestId: contest.contestId },
+            })
+          "
+          >Add Schedule</a
+        >
+      </div>
+      <div>
+        <a
+          href="#"
+          v-on:click="
+            $router.push({
+              name: 'UpdateContest',
+              params: { contestId: contest.contestId },
+            })
+          "
+          >Update</a
+        >
+      </div>
+      <div>
+        <a href="#" v-on:click="deleteContest(contest.contestId)">Delete</a>
+      </div>
+      <div>
+        <a
+          href="#"
+          v-on:click="
+            $router.push({
+              name: 'OverallScores',
+              params: { contestId: contest.contestId },
+            })
+          "
+          >Scores</a
+        >
+      </div>
+    </div> -->
+  </div>
 </template>
 
 <script>
-import ContestsService from '../services/ContestsService';
-import ErrorService from '../services/ErrorService';
+import ContestsService from "../services/ContestsService";
+import ErrorService from "../services/ErrorService";
 export default {
-    created() {
-        this.fetchContests();
-    },
-    methods: {
-        /**
-         * This function accepts an id of a contest and then
-         * calls the ContestService to delete the contest
-         * @param {number} contestId the id of the contest
-         */
-        deleteContest(contestId) {
-            console.log(contestId)
-            if (confirm("Are you sure you want to delete this message? This action cannot be undone.")) {
-                ContestsService.deleteContest(contestId)
-                    .then(response => {
-                        if (response.status === 200) {
-                            this.fetchContests();
-                            this.$router.push({ name: 'contests' });
-                        }
-                    })
-                    .catch(error => ErrorService.handleErrorResponse(error, "deleting"));
+  created() {
+    this.fetchContests();
+  },
+  methods: {
+    /**
+     * This function accepts an id of a contest and then
+     * calls the ContestService to delete the contest
+     * @param {number} contestId the id of the contest
+     */
+    deleteContest(contestId) {
+      console.log(contestId);
+      if (
+        confirm(
+          "Are you sure you want to delete this message? This action cannot be undone."
+        )
+      ) {
+        ContestsService.deleteContest(contestId)
+          .then((response) => {
+            if (response.status === 200) {
+              this.fetchContests();
+              this.$router.push({ name: "contests" });
             }
-        },
-        fetchContests(){
-            ContestsService.fetchContests()
-            .then(response=>{
-                this.$store.commit('SET_CONTESTS', response.data);
-            });
-        },
-    }
-}
+          })
+          .catch((error) =>
+            ErrorService.handleErrorResponse(error, "deleting")
+          );
+      }
+    },
+    fetchContests() {
+      ContestsService.fetchContests().then((response) => {
+        this.$store.commit("SET_CONTESTS", response.data);
+      });
+    },
+  },
+};
 </script>
 
 <style>
 .ContestInfo {
-
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    gap: 20px;
-    /* display: grid;
-    justify-content: center;
-    align-items: center; */
-    background-color: #FFF7D4;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 20px;
+  justify-content: center;
+  align-items: center;
+  margin-left: 15%;
+  margin-right: 15%;
+  background-color: #fff7d4;
 }
 
 .ContestContainer {
+  margin: 4px;
+  border-radius: 5px;
+  padding: 10px;
+  background-color: #ffd95a;
+}
 
-    margin: 4px;
-    border-radius: 5px;
-    padding: 10px;
-    background-color: #FFD95A;
+.btn-container {
+    display: flex;
+    justify-content: space-between;
 }
 </style>
