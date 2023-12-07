6d4de9beb3aa8a2ceb6013bcd18456e5ba5cd8b3
Roland Garza <garza.roland24@gmail.com>
2023-12-07 15:45:18 -0500

    Pushing Schedule Page

--- vue/src/components/Schedule.vue
+++ vue/src/components/Schedule.vue
@@ -1,14 +1,24 @@
 <template>
     <div class="schedule-header">
-    <h1>{{ this.contest.contestName }}</h1> 
-    <h4>{{ this.contest.dateAndTime }}</h4>
+        <h1>{{ this.contest.contestName }}</h1>
+        <h4>{{ this.contest.dateAndTime }}</h4>
     </div>
-  <div class = "schedule-container">
-    <div v-for="schedule in schedule" v-bind:key="schedule.id">
-      <p>{{ schedule.participantName }} : {{ schedule.timeSlot }}</p>
-
+    <div class="schedule-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Participant</th>
+                    <th>Time Slot</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="schedule in schedule" v-bind:key="schedule.id">
+                    <td>{{ schedule.participantName }}</td>
+                    <td>{{ schedule.timeSlot }}</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
-  </div>
 </template>
 
 <script>
@@ -40,7 +50,42 @@ export default {
 .schedule-header {
     text-align: center; 
 }
+
 .schedule-container{
     text-align: center;
+    margin: auto;
+    width: 20%; /* Adjust width as needed */
+}
+
+table {
+    width: 100%;
+    border-collapse: collapse;
+    margin-top: 20px;
+}
+
+th, td {
+    border: 1px solid #ddd;
+    padding: 8px;
+    text-align: left;
+}
+
+th {
+    background-color: #009ee2; 
+    color: whitesmoke;
+    font-weight: bold;
+}
+
+tr:nth-child(even) {
+    background-color:#69cbf536;
+}
+
+tr:nth-child(odd) {
+    background-color: #ffffff; 
+}
+
+tr:hover {
+    background-color: #4fbeee71; /* Color change on hover */
+    transform: scale(1.03); /* Slightly enlarges the row */
+    box-shadow: 0px 0px 10px rgba(0,0,0,0.1); /* Adds a subtle shadow for depth */
 }
 </style>
