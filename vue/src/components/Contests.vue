86c9573ac3493b33f80d15e613455a134391ba00
Forrest Yankey <forrestyankey@gmail.com>
2023-12-13 15:01:19 -0500

    mobile view edits

--- vue/src/components/Contests.vue
+++ vue/src/components/Contests.vue
@@ -1,5 +1,6 @@
 <template>
 <div class="contests-container">
+  <h1 class="title">Contests</h1>  
     <!--SEARCH BAR-->
   <div class="filter-container">
   <form class="d-flex" role="search">
@@ -147,8 +148,14 @@ export default {
 </script>
 
 <style>
+.title{
+  text-align: center;
+  font-family: Lato, sans-serif;
+}
 .contests-container {
   margin-top: 100px;
+  margin-right: 5%;
+  margin-left: 5%;
 }
 .ContestInfo {
   font-family: Lato, sans-serif;
@@ -188,9 +195,10 @@ export default {
 
 .filter-container {
   padding-top: 1%;
-  margin-left: 20%;
-  margin-right: 20%;
+  margin-left: 10%;
+  margin-right: 10%;
   margin-bottom: 1%;
+  
 }
 
 #add-contest {
@@ -220,12 +228,16 @@ body {
 }
 
 @media screen and (max-width: 768px) {
-    .left-links {
-    font-size: 1.1em;
-    }
-
-    .right-links { 
-     font-size: 1.1em; 
+  .filter-container {
+  padding-top: 1%;
+  margin-bottom: 1%;
 }
+
+.contests-container {
+  margin-top: 50px;
+  margin-right: 5%;
+  margin-left: 5%;
+}
+
 }
 </style>
