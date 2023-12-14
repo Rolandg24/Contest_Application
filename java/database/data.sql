6a301e1efdc3785f07b520cac744f3ab25e0060c
Forrest Yankey <forrestyankey@gmail.com>
2023-12-14 11:47:19 -0500

    push db changes

--- java/database/data.sql
+++ java/database/data.sql
@@ -13,7 +13,9 @@ VALUES
 INSERT INTO contests (contest_name, contest_description, contest_date_time, contest_location, category_id, image_url) 
 VALUES 
 ('Columbus Area Band Contest', 'The Columbus Area Band Contest is an exhilarating music competition celebrating the vibrant and diverse local band scene in Columbus, Ohio. This event brings together a wide array of musical talents, from emerging garage bands to more established groups, showcasing a variety of genres including rock, jazz, indie, and more. Participants will compete for the title of "Columbus''s Best Band," along with other exciting prizes. Each band will have a set time to perform their best pieces in front of an enthusiastic audience and a panel of esteemed judges from the music industry. In addition to the musical performances, the event will feature local food trucks, merchandise stalls, and interactive booths, making it a perfect outing for music lovers of all ages.',
- '2024-04-10', 'Columbus, OH', 1, 'https://res.cloudinary.com/dmptbrbof/image/upload/v1702395991/contests_app/gtfgiyvbmn4l3nr8aklb.png');
+ '2024-04-10', 'Columbus, OH', 1, 'https://res.cloudinary.com/dmptbrbof/image/upload/v1702395991/contests_app/gtfgiyvbmn4l3nr8aklb.png'),
+('Tech Elevator Capstone Expo', 'The Tech Elevator Capstone Expo is the culminating event of the 14-week coding BootCamp, where Tech Elevator students showcase their skills and accomplishments through the presentation of full-stack web applications. In this intensive final project, students are divided into teams, each tasked with conceiving, designing, and implementing a comprehensive web application within a tight two-week timeframe. The expo serves as a platform for students to demonstrate their proficiency in various programming languages, frameworks, and technologies learned throughout the BootCamp. Attendees have the opportunity to witness firsthand the innovative solutions and creative applications developed by these teams, highlighting the practical application of their newly acquired coding and development skills. The Capstone Expo not only celebrates the students'' journey and achievements but also provides a valuable opportunity for networking and interaction within the tech community.',
+ '2024-12-15', 'Columbus, OH', 5, 'null');
 
 INSERT INTO participants (participant_name, participant_description, member_count, score, contest_id, image_url, votes) 
 VALUES 
@@ -26,7 +28,21 @@ VALUES
 ('The Groove Makers', 'A funk band known for their groovy basslines and infectious rhythms.', 6, NULL, 1, null, 0),
 ('Lyrical Legends', 'A hip-hop group with sharp lyrics and powerful beats.', 2, NULL, 1, null, 0),
 ('The Serene Strings', 'A classical ensemble bringing a fresh perspective to traditional compositions.', 4, NULL, 1, null, 0),
-('Rhythm Raiders', 'A reggae band infusing classic reggae with modern influences.', 5, NULL, 1, null, 0);
+('Rhythm Raiders', 'A reggae band infusing classic reggae with modern influences.', 5, NULL, 1, null, 0),
+('Team Alpha', NULL, 3, NULL, 2, NULL, 0),
+('Team Bravo', NULL, 3, NULL, 2, NULL, 0),
+('Team Charlie', NULL, 3, NULL, 2, NULL, 0),
+('Team Delta', NULL, 3, NULL, 2, NULL, 0),
+('Team Echo', NULL, 3, NULL, 2, NULL, 0),
+('Team Foxtrot', NULL, 4, NULL, 2, NULL, 0),
+('Team Golf', NULL, 3, NULL, 2, NULL, 0),
+('Team Hotel', NULL, 4, NULL, 2, NULL, 0),
+('Team India', NULL, 4, NULL, 2, NULL, 0),
+('Team Juliett', NULL, 4, NULL, 2, NULL, 0),
+('Team Kilo', NULL, 4, NULL, 2, NULL, 0),
+('Team Lima', NULL, 4, NULL, 2, NULL, 0),
+('Team Mike', NULL, 3, NULL, 2, NULL, 0),
+('Team November', NULL, 4, NULL, 2, NULL, 0);
 
 INSERT INTO time_slots (contest_id, participant_id, time_slot)
 VALUES
@@ -40,11 +56,5 @@ VALUES
 (1, 8, '15:00:00'),
 (1, 9, '16:00:00'),
 (1, 10, '17:00:00');
-
-
--- INSERT INTO overall_scores (contest_id, participant_id, overall_score)
--- VALUES
--- (1, 14, 90)
-
 
 COMMIT TRANSACTION;
